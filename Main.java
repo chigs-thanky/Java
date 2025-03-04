1f8db6b4087ccc0274036b76effad05cf2404e69
Chirag Thanki <149762309+chigs-thanky@users.noreply.github.com>
2025-03-04 15:47:25 +0530

    First JAVA Program

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+public static void main(String[] args){
+    System.out.println("Hello World!");
+    //Note: Println() adds a new line to for each method ( Method = function(), in JAVA, a function() is called a method. )
+    System.out.println("Hello Chigs, good to see you!");
+
+    //The diffrenece between print() and println() method is: print() doesn't add new line while println() does.
+    System.out.print("Hello ");
+    System.out.print("Chigs");
+}
+}

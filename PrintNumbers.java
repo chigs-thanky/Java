a58ab63cf3eca7cd06973358142ed96ad1c657c4
Chirag Thanki <149762309+chigs-thanky@users.noreply.github.com>
2025-03-04 16:10:18 +0530

    Variables

---
+++ PrintNumbers.java
@@ -0,0 +1,6 @@
+public class PrintNumbers {
+    public static void main(String[] args){
+        System.out.println("2+5"); //This is a string because it's wrapped inside double quotes
+        System.out.println(2+5); //This is a Mathematical operation because it isn't wrapped inside double quotes
+    }
+}
---
+++ Variables.java
@@ -0,0 +1,20 @@
+public class Variables {
+    public static void main(String[] args){
+        //String DataType
+        String name = "Chirag";
+        System.out.println(name);
+
+        //Int DataType
+        int myDate = 12;
+        System.out.println(myDate);
+
+        //Float DataType
+        float weight = 65.67f;
+        System.out.println(weight);
+
+        //Char DataType
+        char initial = 'C';
+        System.out.println(initial);
+    
+    }
+}

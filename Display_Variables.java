28de1235096e8fadebdf9d516c8f75ebb9bd68fa
Chirag Thanki <149762309+chigs-thanky@users.noreply.github.com>
2025-03-06 15:19:17 +0530

    Define multiple variable

---
+++ Display_Variables.java
@@ -0,0 +1,14 @@
+public class Display_Variables {
+    public static void main(String[] args){
+        String name = "John";
+        System.out.println(name);
+        //Now to combine text to print with variable, use + sign
+        System.out.println("Hello, " + name);
+
+        //We can combine strings using + sign also
+        String fName = "Chigs";
+        String lName = " Thanky";
+        String fullName = fName + lName;
+        System.out.println(fullName);
+    }
+}

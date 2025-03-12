55f90380a2b0ca23b64bb3ca08fbc8a57fece26d
Chirag Thanki <149762309+chigs-thanky@users.noreply.github.com>
2025-03-12 11:51:17 +0530

    String and Operators

---
+++ Operators.java
@@ -0,0 +1,18 @@
+public class Operators {
+    public static void main(String[] args) {
+        int sum1 = 10 + 5;
+        System.out.println(sum1);
+        int sum2 = sum1 + 5;
+        System.out.println(sum2);
+        int sum3 = sum1 + sum2;
+        System.out.println(sum3);
+        
+        int x = 50, y = 10;
+        System.out.println("x = " + x + "\ny = " + y);
+        System.out.println("x/y: " + x / y);
+        x++;
+        System.out.println(x);
+
+    }
+
+}
---
+++ String_Intro.java
@@ -0,0 +1,15 @@
+public class String_Intro {
+    public static void main(String[] args) {
+        String name = "Chigs";
+        System.out.println(name);
+
+        //Used to find the length of the string
+        System.out.println(name.length());
+
+        // Used to perform method to Upper string letters
+        System.out.println(name.toUpperCase());
+
+        //Used to perform method to lower string letters
+        System.out.println(name.toLowerCase());
+    }
+}

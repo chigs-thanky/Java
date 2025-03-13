979ea0146d466322496296b7ee597386bc88b6aa
Chirag Thanki <149762309+chigs-thanky@users.noreply.github.com>
2025-03-13 15:14:42 +0530

    Multiplication Table

---
+++ Multiplication_Table.java
@@ -0,0 +1,8 @@
+public class Multiplication_Table {
+    public static void main(String[] args) {
+        int num = 5;
+        for (int i = 1; i < 11; i++) {
+            System.out.println(num + " X " + i + " = " + num * i);
+        }
+    }
+}

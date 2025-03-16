00ffcce853c29993dc599893657f79d58545949c
chigs-thanky <chigsthanky@outlook.com>
2025-03-16 12:46:04 +0530

    Loop through Arrays

---
+++ Multi_Dimensional_Array.java
@@ -0,0 +1,10 @@
+
+public class Multi_Dimensional_Array {
+
+    public static void main(String[] args) {
+        int[][] myNumbers = {{1, 2, 3, 4}, {5, 6, 7}};
+        String[][] names = {{"Chirag", "Janki"}, {"Poorvi", "Rupal", "Pratik"}};
+        System.out.println(myNumbers[1][0]);
+        System.out.println(names[1][1]);
+    }
+}

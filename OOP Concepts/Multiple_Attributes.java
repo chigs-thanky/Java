ec2e565cccc54284f2959fa0287eb3064bc89208
Chirag Thanki <149762309+chigs-thanky@users.noreply.github.com>
2025-06-10 13:19:40 +0530

    Multiple Attributes

---
+++ OOP Concepts/Multiple_Attributes.java
@@ -0,0 +1,15 @@
+
+public class Multiple_Attributes {
+
+    String fname = "John";
+    String firstName = "Chirag";
+    String lname = "Doe";
+    int age = 24;
+    String surname = "Thanky";
+
+    public static void main(String[] args) {
+        Multiple_Attributes myObj = new Multiple_Attributes();
+        System.out.println("Name: " + myObj.firstName + " " + myObj.surname);
+        System.out.println("Age: " + myObj.age);
+    }
+}

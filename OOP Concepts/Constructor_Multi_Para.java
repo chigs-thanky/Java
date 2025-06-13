b9a2d8b29b75586d02e8ab8910c813e512b1e89c
Chirag Thanki <149762309+chigs-thanky@users.noreply.github.com>
2025-06-13 15:23:27 +0530

    Constructor Multiparameters

--- OOP Concepts/Constructor_Multi_Para.java
+++ OOP Concepts/Constructor_Multi_Para.java
@@ -4,9 +4,15 @@ public class Constructor_Multi_Para {
     int Launchyear;
     String modelName;
 
+    int birthDate;
+    String name;
+
     public Constructor_Multi_Para(int year, String model) {
         Launchyear = year;
         modelName = model;
+
+        birthDate = year;
+        name = model;
     }
 
     public static void main(String[] args) {
@@ -16,5 +22,7 @@ public class Constructor_Multi_Para {
         System.out.println(myObj.Launchyear + " - " + myObj.modelName);
         System.out.println(myObj.Launchyear + " - " + myObj2.modelName);
         System.out.println(myObj.Launchyear + " - " + myObj3.modelName);
+        Constructor_Multi_Para myBirth = new Constructor_Multi_Para(1997, "Chirag");
+        System.out.println(myBirth.birthDate + " - " + myBirth.name);
     }
 }

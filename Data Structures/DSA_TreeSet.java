1142941067f299b70a0f64a89f62170530f9cfff
Chirag Thanki <149762309+chigs-thanky@users.noreply.github.com>
2025-07-23 15:56:25 +0530

    TreeSet

--- Data Structures/DSA_TreeSet.java
+++ Data Structures/DSA_TreeSet.java
@@ -6,6 +6,7 @@ import java.util.TreeSet;
 public class DSA_TreeSet {
 
     public static void main(String[] args) {
+        //For Strings (Natual Order)
         TreeSet<String> names = new TreeSet<String>();
         names.add("Dipak");
         names.add("Chirag");
@@ -15,5 +16,22 @@ public class DSA_TreeSet {
         names.add("Ravi");
         names.add("Punit");
         System.out.println(names);
+
+        for (String Name : names) {
+            System.out.println(Name);
+        }
+
+        //For numbers (Natural Order)
+        TreeSet<Integer> myNum = new TreeSet<Integer>();
+        myNum.add(12);
+        myNum.add(29);
+        myNum.add(37);
+        myNum.add(78);
+        myNum.add(9);
+        System.out.println(myNum);
+
+        for (int i : myNum) {
+            System.out.println(i);
+        }
     }
 }

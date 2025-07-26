8f142a1e45ecb379970f142c824abb87f6e95ddd
Chirag Thanki <149762309+chigs-thanky@users.noreply.github.com>
2025-07-26 19:42:51 +0530

    HashMap Other DataTypes

---
+++ Data Structures/DSA_HashMap_DataTypes.java
@@ -0,0 +1,39 @@
+
+import java.util.HashMap; //Import HashMap class
+
+public class DSA_HashMap_DataTypes {
+
+    public static void main(String[] args) {
+        HashMap<String, Integer> people = new HashMap<String, Integer>();
+        //Add keys and values (Name-String, Birthdate-Integer)
+        people.put("Chirag", 30);
+        people.put("Bunny", 17);
+        people.put("Sid", 24);
+        people.put("Daniel", 07);
+        people.put("John", 19);
+        System.out.println(people); //Output in one line
+        // If you want output each values in separate rows, use for-each loop
+
+        //Use keySet() to print key values
+        for (String keys : people.keySet()) {
+            System.out.println(keys);
+        }
+        //Use values() to print values of respective keys
+
+        // for (String value : people.values()) { 
+        //generate error as value is an int while you defined it as a String
+        //Corrected code line
+        for (int value : people.values()) {
+            System.out.println(value);
+        }
+
+        //To print both keys and values in loop
+        for (String key_value : people.keySet()) {
+            System.out.println(key_value + ", " + people.get(key_value));
+        }
+        // To print both keys and values with extra info
+        for (String key_value : people.keySet()) {
+            System.out.println("Key: " + key_value + ", value: " + people.get(key_value));
+        }
+    }
+}

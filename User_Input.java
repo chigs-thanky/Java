dc5e91ef69233a73c2d12795de416dbbc5d008d1
Chirag Thanki <149762309+chigs-thanky@users.noreply.github.com>
2025-07-26 19:47:30 +0530

    User Input file updation

--- User_Input.java
+++ User_Input.java
@@ -4,13 +4,11 @@ import java.util.Scanner; //Import Scanner Class
 public class User_Input { //This class name should match the file name, here ("User_Input.java")
 
     public static void main(String[] args) {
-        System.out.println("Hello Class 8th\n");
-
         Scanner myNum = new Scanner(System.in); //Create scanner object
         // System.out.println("Enter a Number: "); //To enter number on next line
         System.out.print("Enter a Number: "); //To enter number on same  line
         int num = myNum.nextInt(); //Read user input - nextInt() - for taking integer input, for string we have to use nextLine(), for double use nextDouble() 
-        System.out.println("\n" + num + "'s Table:\n");
+        // System.out.println("\n" + num + "'s Table:\n");
         for (int i = 1; i <= 10; i++) {
             System.out.println(" " + num + " X " + i + " = " + num * i);
         }

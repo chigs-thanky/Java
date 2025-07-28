866626c033cd3633c527f5a4ad868b532154ef92
Chirag Thanki <149762309+chigs-thanky@users.noreply.github.com>
2025-07-28 13:20:18 +0530

    Wrapper Class

---
+++ Wrapper_Class.java
@@ -0,0 +1,13 @@
+
+public class Wrapper_Class {
+
+    public static void main(String[] args) {
+        Integer myInt = 100;
+        Long moNum = 9265090051L;
+        String myString = myInt.toString();
+        System.out.println(myString.length());
+        String numString = moNum.toString();
+        System.out.println(moNum);
+        System.out.println(numString.length());
+    }
+}
